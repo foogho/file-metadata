a7c2d0e871a8d31c2841d9ad7a80b80d4efc0849
Ali Ebrahimi <aliebrahimi2150@gmail.com>
2023-04-23 00:33:26 +0330

    feat: add post file endpoint

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var multer = require('multer')();
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/api/file', (req, res) => {
-  res.json({ message: 'reading file ...' });
-});
+app.post('/api/file', [
+  multer.single('upfile'),
+  (req, res) => {
+    const { originalname: name, mimetype: type, size } = req.file;
+    res.json({
+      name,
+      type,
+      size
+    });
+  },
+]);
 
 module.exports = app;
